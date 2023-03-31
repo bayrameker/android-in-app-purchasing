a11ba2aef8872d817f0e5e2ec682682abd84766d
bayrameker <bayram.eker@eltturkey.com>
2023-03-31 09:27:36 +0300

    first

---
+++ app/src/main/java/com/bayram/inappexample/Subscription.java
@@ -0,0 +1,252 @@
+package com.bayram.inappexample;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.billingclient.api.AcknowledgePurchaseParams;
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.ProductDetails;
+import com.android.billingclient.api.ProductDetailsResponseListener;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.QueryProductDetailsParams;
+import com.android.billingclient.api.QueryPurchasesParams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subscription extends AppCompatActivity {
+
+    private static String PRODUCT_PREMIUM = "yearly";
+    private static String PRODUCT_MONTHLY = "monthly";
+    private BillingClient billingClient;
+
+    Button btn_premium, btn_restore;
+    TextView tv_status;
+    private ArrayList<String> purchaseItemIDs = new ArrayList<String>() {{
+        add(PRODUCT_PREMIUM);
+        add(PRODUCT_MONTHLY);
+    }};
+    private String TAG = "iapSample";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscription);
+        billingClient = BillingClient.newBuilder(this)
+                .enablePendingPurchases()
+                .setListener(
+                        (billingResult, list) -> {
+
+                            if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && list != null) {
+                                for (Purchase purchase : list) {
+                                    verifySubPurchase(purchase);
+                                }
+                            }
+                        }
+                ).build();
+
+        //start the connection after initializing the billing client
+        establishConnection();
+        init();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        billingClient.queryPurchasesAsync(
+                QueryPurchasesParams.newBuilder().setProductType(BillingClient.ProductType.SUBS).build(),
+                (billingResult, list) -> {
+                    if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                        for (Purchase purchase : list) {
+                            if (purchase.getPurchaseState() == Purchase.PurchaseState.PURCHASED && !purchase.isAcknowledged()) {
+                                verifySubPurchase(purchase);
+                            }
+                        }
+                    }
+                }
+        );
+    }
+
+    void init() {
+        btn_premium = this.findViewById(R.id.btn_premium);
+        btn_restore = this.findViewById(R.id.btn_restore);
+        tv_status = this.findViewById(R.id.tv_status);
+
+        btn_premium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GetSubPurchases();
+            }
+        });
+
+        btn_restore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                restorePurchases();
+            }
+        });
+    }
+
+    void establishConnection() {
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                    // The BillingClient is ready. You can query purchases here.
+                    //Use any of function below to get details upon successful connection
+                    Log.d("TAG", "Connection Established");
+                }
+            }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+                Log.d("TAG", "Connection NOT Established");
+                establishConnection();
+            }
+        });
+    }
+
+    void GetSubPurchases() {
+        ArrayList<QueryProductDetailsParams.Product> productList = new ArrayList<>();
+
+        productList.add(
+                QueryProductDetailsParams.Product.newBuilder()
+                        .setProductId(PRODUCT_PREMIUM)
+                        .setProductType(BillingClient.ProductType.SUBS)
+                        .build()
+        );
+
+        QueryProductDetailsParams params = QueryProductDetailsParams.newBuilder()
+                .setProductList(productList)
+                .build();
+
+
+        billingClient.queryProductDetailsAsync(params, new ProductDetailsResponseListener() {
+            @Override
+            public void onProductDetailsResponse(@NonNull BillingResult billingResult, @NonNull List<ProductDetails> list) {
+                LaunchSubPurchase(list.get(0));
+                Log.d("TAG", "Product Price" + list.get(0).getSubscriptionOfferDetails().get(0).getPricingPhases().getPricingPhaseList().get(0).getFormattedPrice());
+
+            }
+        });
+    }
+    //Call this function using PRODUCT_PREMIUM or PRODUCT_MONTHLY as parameters.
+    void GetListsSubDetail(String SKU) {
+        ArrayList<QueryProductDetailsParams.Product> productList = new ArrayList<>();
+
+        //Set your In App Product ID in setProductId()
+        for (String ids : purchaseItemIDs) {
+            productList.add(
+                    QueryProductDetailsParams.Product.newBuilder()
+                            .setProductId(ids)
+                            .setProductType(BillingClient.ProductType.SUBS)
+                            .build());
+        }
+
+        QueryProductDetailsParams params = QueryProductDetailsParams.newBuilder()
+                .setProductList(productList)
+                .build();
+
+        billingClient.queryProductDetailsAsync(params, new ProductDetailsResponseListener() {
+            @Override
+            public void onProductDetailsResponse(@NonNull BillingResult billingResult, @NonNull List<ProductDetails> list) {
+//                Log.d(TAG, "Total size is: " + list);
+
+                for (ProductDetails li : list) {
+                    if (li.getProductId().equalsIgnoreCase(SKU) && SKU.equalsIgnoreCase(PRODUCT_MONTHLY)) {
+                        LaunchSubPurchase(li);
+                        Log.d(TAG, "Monthly Price is " + li.getSubscriptionOfferDetails().get(0).getPricingPhases().getPricingPhaseList().get(0).getFormattedPrice());
+                        return;
+                    } else if (li.getProductId().equalsIgnoreCase(SKU) && SKU.equalsIgnoreCase(PRODUCT_PREMIUM)) {
+                        LaunchSubPurchase(li);
+                        Log.d(TAG, "Yearly Price is " + li.getSubscriptionOfferDetails().get(0).getPricingPhases().getPricingPhaseList().get(0).getFormattedPrice());
+                        return;
+                    }
+
+                }
+                //Do Anything that you want with requested product details
+            }
+        });
+    }
+    void LaunchSubPurchase(ProductDetails productDetails) {
+        assert productDetails.getSubscriptionOfferDetails() != null;
+        ArrayList<BillingFlowParams.ProductDetailsParams> productList = new ArrayList<>();
+
+        productList.add(
+                BillingFlowParams.ProductDetailsParams.newBuilder()
+                        .setProductDetails(productDetails)
+                        .setOfferToken(productDetails.getSubscriptionOfferDetails().get(0).getOfferToken())
+                        .build()
+        );
+
+        BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+                .setProductDetailsParamsList(productList)
+                .build();
+
+        billingClient.launchBillingFlow(this, billingFlowParams);
+    }
+
+    void verifySubPurchase(Purchase purchases) {
+        if (!purchases.isAcknowledged()) {
+            billingClient.acknowledgePurchase(AcknowledgePurchaseParams
+                    .newBuilder()
+                    .setPurchaseToken(purchases.getPurchaseToken())
+                    .build(), billingResult -> {
+
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                    for (String pur : purchases.getProducts()) {
+                        if (pur.equalsIgnoreCase(PRODUCT_PREMIUM)) {
+                            Log.d("TAG", "Purchase is successful" + pur);
+                            tv_status.setText("Yay! Purchased");
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    void restorePurchases() {
+
+        billingClient = BillingClient.newBuilder(this).enablePendingPurchases().setListener((billingResult, list) -> {
+        }).build();
+        final BillingClient finalBillingClient = billingClient;
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingServiceDisconnected() {
+            }
+
+            @Override
+            public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
+
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                    finalBillingClient.queryPurchasesAsync(
+                            QueryPurchasesParams.newBuilder().setProductType(BillingClient.ProductType.SUBS).build(), (billingResult1, list) -> {
+                                if (billingResult1.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                                    if (list.size() > 0) {
+                                        for (int i = 0; i < list.size(); i++) {
+                                            if (list.get(i).getProducts().contains(PRODUCT_PREMIUM)) {
+                                                tv_status.setText("Premium Restored");
+                                                Log.d("TAG", "Product id " + PRODUCT_PREMIUM + " will restore here");
+                                            }
+                                        }
+                                    } else {
+                                        tv_status.setText("Nothing found to Restored");
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+}
